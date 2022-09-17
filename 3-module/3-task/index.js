13c303941f3b0137e186e1a77df06bbfe1a2cbe7
Stan Mar <nordgroup812@gmail.com>
2022-09-17 09:50:14 +0300

    3.1 - 3.5

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,7 +1,3 @@
 function camelize(str) {
-  let func = (elem) => elem[0].toUpperCase() + elem.slice(1);
-  if (str == '') {return '';}
-  if (str[0] == '-') {return str.slice(1).split('-').map(func).join('');}
-  str = str.split('-').map(func).join('');
-  return str[0].toLowerCase() + str.slice(1);
+  return str.split('-').map((elem, index) => index !== 0 ? elem[0].toUpperCase() + elem.slice(1) : elem).join('');
 }
