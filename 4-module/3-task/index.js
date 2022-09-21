ce3f67d948356830eacb28edebfd9e4bc1a41386
Stan Mar <nordgroup812@gmail.com>
2022-09-21 15:20:45 +0300

    4.1 - 4.3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,20 @@
 function highlight(table) {
-  // ваш код...
+  for (let elem of table.rows) {
+    let elChange = elem.cells[3];
+    if (elChange.hasAttribute('data-available')) {
+      if (elChange.dataset.available == "true") { elem.classList.add('available');}
+    } 
+    if (elChange.hasAttribute('data-available')) {
+      if (elChange.dataset.available == "false") { elem.classList.add('unavailable');}
+    } 
+    if (!elChange.hasAttribute('data-available')) {elem.setAttribute('hidden', 'hidden');}
+    //else {elem.setAttribute('hidden', 'hidden');}
+
+    if (elem.cells[2].textContent == 'm') {elem.classList.add("male");}
+    if (elem.cells[2].textContent == 'f') {elem.classList.add("female");}
+
+    if (Number(elem.cells[1].textContent) < 18) {elem.style.textDecoration = "line-through";}
+  }
+
 }
+
