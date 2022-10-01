419282c9529f2513587a6f52579c1ccc4d150d2d
Stan Mar <nordgroup812@gmail.com>
2022-10-01 21:24:42 +0300

    переделал 5.2

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,7 +1,6 @@
 function toggleText() {
   let div = document.querySelector("#text"); 
   document.querySelector(".toggle-text-button").addEventListener('click', () => { 
-    div.classList.toggle('off'); 
-    div.classList.contains('off') ?  div.hidden = true : div.hidden = false; 
+    div.hasAttribute('hidden') ? div.removeAttribute('hidden') : div.setAttribute('hidden', 'hidden');
   });
 }
